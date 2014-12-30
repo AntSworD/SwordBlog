3234f4b0fba06ed2207d46908736d68ea9de7b93
AntSworD <zhengjj.asd@gmail.com>
2014-12-30 14:06:00 +0000

    Add article router

---
+++ routes/article.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET article page. */
+router.get('/', function(req, res) {
+  res.end('This is Article Page.');
+});
+
+/* GET article page with id. */
+router.get('/:id', function(req, res) {
+  var id = req.params.id;
+  res.end('This is Article Page, the article\'s id is ' + id);
+});
+
+module.exports = router;
