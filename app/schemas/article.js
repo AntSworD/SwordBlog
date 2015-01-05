92c17b7d1fb224822b89f6cda1de4df0ead77048
AntSworD <zhengjj.asd@gmail.com>
2015-01-05 11:40:31 +0000

    Append method to  ArticleSchema

--- app/schemas/article.js
+++ app/schemas/article.js
@@ -52,3 +52,30 @@ var ArticleSchema = new Schema({
     }
   }
 });
+
+ArticleSchema.pre('save', function(next) {
+  if (this.isNew) {
+    this.meta.publishAt = this.meta.updateAt = Date.now();
+  } else {
+    this.meta.updateAt = Date.now();
+  }
+
+  next();
+});
+
+ArticleSchema.statics = {
+  fetch: function(cb) {
+    return this
+      .find({})
+      .sort('meta.updateAt')
+      .exec(cb);
+  },
+  findById: function(id, cb) {
+    return this
+      .findOne({_id: id})
+      .sort('meta.updateAt')
+      .exec(cb);
+  }
+};
+
+module.exports = ArticleSchema;
