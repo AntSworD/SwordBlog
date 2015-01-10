e82a78f886cedb82841cdda1678210c9b82144f9
AntSworD <zhengjj.asd@gmail.com>
2015-01-10 16:59:46 +0000

    模拟数据跑通博客文章页面

--- routes/article.js
+++ routes/article.js
@@ -3,8 +3,16 @@ var router = express.Router();
 
 /* GET article page. */
 router.get('/', function(req, res) {
+  var article = {
+    title: '第一篇文章',
+    authorId: 'AntSworD', // TODO:这里改成authorName
+    meta: Date.now(),
+    pv: 12345,
+    content: '文章内容'
+  };
   res.render('article', {
     title: 'SwordBlog',
+    article: article
   });
 });
 
